4520da2ba1ee2c353f95b36bddccea94139ed64a
holinmark70@gmail.com <holinmark70@gmail.com>
2022-10-28 06:10:52 -0400

    Deleted old file XFileParser.h XFileParser.cpp and renamed WindowsTemplate.h WindowsTemplate.cpp to XFileParser.h XFileParser.cpp

--- XFileParser/WindowsTemplate.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "resource.h"
-
-int APIENTRY wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int);
--- XFileParser/XFileParser.h
+++ XFileParser/XFileParser.h
@@ -1,26 +1,5 @@
 #pragma once
-/*
-msbuild /m:4 /p:Configuration=Debug XFileParser.sln
-*/
 
-#include <Windows.h>
-#include <DirectXMath.h>
-#include <fstream>
-#include <cwchar>
-#include "sMaterialList.h"
-#include "sFrameList.h"
-#include "sMeshList.h"
-#include "cTextXFileParser.h"
-#include "cXFile.h"
+#include "resource.h"
 
-void PrintMatrix(DirectX::XMFLOAT4X4);
-void PrintMaterials(std::vector<ns_HoLin::sMaterial>&, ns_HoLin::sMesh*, const char*);
-void PrintMesh(ns_HoLin::sMeshList*);
-void PrintFrames(ns_HoLin::sSequenceOfFrames*);
-void PrintAnimationSet(ns_HoLin::sAnimationSetList*);
-void PrintDuplicates(ns_HoLin::sMesh*);
-void PrintData(ns_HoLin::cTextXFileParser*);
-void OpenFileWithMeshFileName(ns_HoLin::cXFile&);
-BOOL ReadMeshFile(ns_HoLin::cTextXFileParser*);
-BOOL GetXFileHeader(ns_HoLin::cTextXFileParser*, BOOL&, BOOL&);
-int wmain(DWORD, const wchar_t**);
+int APIENTRY wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int);
